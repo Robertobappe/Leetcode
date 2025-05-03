f24c6cf1240dbcb7e8ad317e2c584193ba7a8f91
Roberto Silva <robertobs@usp.br>
2025-05-02 21:54:45 -0300

    vf

--- maximumSubarray.js
+++ maximumSubarray.js
@@ -1,8 +1,5 @@
-//Brute force
+//Brute force --> 
 function maxSubArray(nums){
-    if(nums.length == 1){
-        return nums[0];
-    }
     const arr = [0];
     let soma = 0;
 
@@ -11,6 +8,7 @@ function maxSubArray(nums){
         for(let j=i+1; j < nums.length;j++){
             somaj += nums[j];
             soma = nums[i] + somaj; 
+
             if(arr[0] == '' || soma > arr[0]){
                 arr[0] = soma; 
             }
@@ -18,7 +16,19 @@ function maxSubArray(nums){
     }
     return arr[0];
 }
-console.log(maxSubarray([-2,1,-3,4,-1,2,1,-5,4]));
 
-//nums[i] = 4
-//nums[j] = -1
+//Better solution
+function maxSubArray2(nums){
+    let curSum = nums[0]; 
+    let maxSub = nums[0]; 
+
+    if(nums.length == 1){
+        return nums[0];
+    }     
+    for (let i=1; i < nums.length; i++){
+        curSum = Math.max(nums[i], nums[i] + curSum);
+        maxSub = Math.max(maxSub, curSum);  
+    }
+    return maxSub;
+}
+console.log(maxSubArray2([-2,1,-3,4,-1,2,1,-5,4]));
