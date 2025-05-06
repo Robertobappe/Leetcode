117006a3ca9c505f28dd913e3e6d6d1fd9d5aab7
Roberto Silva <robertobs@usp.br>
2025-05-05 23:26:28 -0300

    vf

---
+++ validAnagram.js
@@ -0,0 +1,9 @@
+function validAnagram(s,t){
+    if (s.length !== t.length){
+        return false;
+    }
+    let sSort = s.split("").sort().join();
+    let tSort = t.split("").sort().join();
+    return sSort == tSort;
+}
+console.log(validAnagram('anagram','nagaram'));
