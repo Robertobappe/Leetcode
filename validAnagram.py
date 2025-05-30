cf9eda2d596be3d493230e26780f3276e15c7d7d
Roberto Silva <robertobs@usp.br>
2025-05-30 00:01:03 -0300

    v3

--- validAnagram.py
+++ validAnagram.py
@@ -1,8 +1,11 @@
+#Better solution
 def validAnagram(s,t):
     if(len(s) != len(t)):
         return False
-    s.sor()
-    print(s)
-    return None
-
-print(validAnagram('anagram','nagaram'))
+    s,t = list(s),list(t)
+    s.sort()
+    t.sort()
+    sOrdenada = "".join(s)
+    tOrdenada = "".join(s)
+    if(sOrdenada == tOrdenada):
+        return True
