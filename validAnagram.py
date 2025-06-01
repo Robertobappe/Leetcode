cc0114fe7938dfcdc9be633b6ac53ab496c4b424
Roberto Silva <robertobs@usp.br>
2025-05-31 21:38:46 -0300

    vf

--- validAnagram.py
+++ validAnagram.py
@@ -1,11 +1,35 @@
-#Better solution
-def validAnagram(s,t):
-    if(len(s) != len(t)):
-        return False
-    s,t = list(s),list(t)
-    s.sort()
-    t.sort()
-    sOrdenada = "".join(s)
-    tOrdenada = "".join(s)
-    if(sOrdenada == tOrdenada):
-        return True
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        #Solution
+        """        
+        mapS,mapT = {}, {}
+
+        for i in range(len(s)):
+            mapS[s[i]] = 1 + mapS.get(s[i], 0)
+            mapT[t[i]] = 1 + mapT.get(t[i], 0)
+        
+        return mapS == mapT """
+        #Dicionários em Python são Comparados por Conteúdo, Não por Ordem        
+        #Time complexity O(n)
+        #Space complexity O(k) --> since we have at most 26 different characters 
+    
+        #Other solution
+        """
+        return sorted(s) == sorted(t)
+        """
+        #Time complexity O(nlogn)
+        #Space complexity O(1) or O(n)
+
+        #Other solution
+        from collections import Counter
+        #cria um dicionário que mapeia cada caractere de s ao seu número de ocorrências
+        return Counter(s) == Counter(t)
+        #Time Complexity O(n)
+        #Space Complexity O(k)
+
+sol = Solution() # Step 1: Create an instance
+result = sol.isAnagram("anagram","nagaram")
+print(result)
