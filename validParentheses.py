914f0055c10a8d36973e0fe9b3e1f3fc7f20e57e
Roberto Silva <robertobs@usp.br>
2025-06-03 23:44:51 -0300

    v1

---
+++ validParentheses.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = deque()
+        for i in s:
+            if i == '(' or i == '{' or i == '[':
+                stack.append(i)
+            elif len(stack) > 0 and stack[-1] == '(' and i == ')' or len(stack) > 0 and stack[-1] == '[' and i == ']' or len(stack) > 0 and stack[-1] == '{' and i == '}':
+                stack.pop()
+            else:
+                return False
+        if len(stack) == 0:
+            return True
+        return False
+
+sol = Solution()
+rel = sol.isValid("()")
+print('rel ', rel)
