c2b7366dd2fbb3b4f9b1097da24484a9b448c4e4
Roberto Silva <robertobs@usp.br>
2025-06-05 00:40:46 -0300

    vf

--- validParentheses.py
+++ validParentheses.py
@@ -16,17 +16,23 @@ class Solution:
         return False'''
 
         #Another solution
-        mapC = {')':'(','}':'{',']':'['}
+        closeToOpen = {')':'(','}':'{',']':'['}
         for i in s:
-            if mapC[i]:
-                stack.pop()
-            stack.append(i)
-        if len(stack) == 0:
-            return True
-        return False
-
+            #check if i is a closing string
+            if i in closeToOpen:
+                #check if stack is not empity
+                #check if 'close' string matches with 'open' string
+                if stack and closeToOpen[i] == stack[-1]:
+                    stack.pop()
+                ##check if i is a closing string and the stack is empity
+                else:
+                    return False
+            #if i is not in closeToOpen, it's a open string
+            else:
+                stack.append(i)
+        return True if not stack else False
     
 
 sol = Solution()
-rel = sol.isValid("()")
+rel = sol.isValid("()[]{}")
 print('rel ', rel)
