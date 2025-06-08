13dbe2050158ef5c93a8d7fd9da45ca03232ee14
Roberto Silva <robertobs@usp.br>
2025-06-08 17:57:39 -0300

    vf

--- twoSumII.py
+++ twoSumII.py
@@ -2,12 +2,26 @@ from typing import List
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        #Solution usuing space complexity of O(n)
+        '''
         mapNum = {}
         for index,value in enumerate(numbers):
             complement = target - value
             if complement in mapNum:
                 return [mapNum[complement] + 1, index + 1]
-            mapNum[value] = index
+            mapNum[value] = index'''
+        
+        #Solution with space complexity of O(1)
+        l,r = 0, len(numbers) - 1
+        
+        while l < r:
+            sumPointers = numbers[l] + numbers[r]
+            if sumPointers > target:
+                r -= 1
+            elif sumPointers < target:
+                l += 1
+            else:
+                return [l+1,r+1]
 
 def main():
     test_cases = [
