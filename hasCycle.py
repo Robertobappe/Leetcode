07db3443c785249d97951455912c1e5cb56e66c9
Roberto Silva <robertobs@usp.br>
2025-06-29 20:30:05 -0300

    vf

--- hasCycle.py
+++ hasCycle.py
@@ -8,6 +8,7 @@ class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         # Time  complexity O(n)
         # Space complexity O(n)
+        '''
         seen_nodes  = set()
         current = head
         iteration = True
@@ -23,5 +24,18 @@ class Solution:
                 if current.next == None:
                     return False
                 iteration = True
-                current = current.next
+                current = current.next'''
+        
+        # Floyd's Tortoise and Hare
+        # Time complexity O(n)
+        # Space complexity O(1)
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        
+        return False
         
