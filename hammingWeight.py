3bf125f8b94ec73ead7520b2b0ee1db23d9eb71b
Roberto Silva <robertobs@usp.br>
2025-07-01 22:39:47 -0300

    vf

--- hammingWeight.py
+++ hammingWeight.py
@@ -1,14 +1,37 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
+        # My solution
+        # Space complexity O(32 btis) = O(c), where c is a constant
+        # Verify each bit
         binaryNumber = bin(n)[2:]
         sumOne = 0
 
+        # Time complexity  O(32 btis) = O(c)
         for i in range(len(binaryNumber)):
             if binaryNumber[i] == '1':
                 sumOne += 1
             
         return sumOne
     
+
+        # Another solution 1
+        # Verify each bit
+        '''
+        res = 0
+        while n:
+            res += n % 2
+            n = n >> 1
+        return res'''
+    
+    
+        # Another solution 2: most efficient
+        # Verify just the 1 bit
+        '''
+        res = 0 
+        while n:
+            n &= (n - 1)
+            res += 1
+        return res'''    
 
 
 def main():
